bb512d2c2fc61b0ac59d7ab97c0e2b48ed11ff61
Radovan Bast <bast@users.noreply.github.com>
2016-09-23 20:16:25 +0200

    routinely test the generate script

--- cmake/custom/test.cmake
+++ cmake/custom/test.cmake
@@ -1,17 +1,17 @@
 enable_testing()
 
-add_test(main_test py.test -vv -s ${PROJECT_SOURCE_DIR}/test/test.py)
+add_test(test_main py.test -vv -s ${PROJECT_SOURCE_DIR}/test/test.py)
 
 set_property(
     TEST
-        main_test
+        test_main
     PROPERTY
         ENVIRONMENT PROJECT_BUILD_DIR=${PROJECT_BINARY_DIR}
     )
 
 set_property(
     TEST
-        main_test
+        test_main
     APPEND
     PROPERTY
         ENVIRONMENT PROJECT_INCLUDE_DIR=${PROJECT_SOURCE_DIR}/api
@@ -19,8 +19,10 @@ set_property(
 
 set_property(
     TEST
-        main_test
+        test_main
     APPEND
     PROPERTY
         ENVIRONMENT PYTHONPATH=${PROJECT_SOURCE_DIR}/api
     )
+
+add_test(test_generate py.test -vv -s ${PROJECT_SOURCE_DIR}/src/generate.py)
