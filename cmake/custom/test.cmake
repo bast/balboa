586d635b61d1561c4f8e8bc04f5860f7537cd518
Radovan Bast <bast@users.noreply.github.com>
2016-09-23 20:15:39 +0200

    rename

--- cmake/custom/test.cmake
+++ cmake/custom/test.cmake
@@ -1,17 +1,17 @@
 enable_testing()
 
-add_test(pytest py.test -vv -s ${PROJECT_SOURCE_DIR}/test/test.py)
+add_test(main_test py.test -vv -s ${PROJECT_SOURCE_DIR}/test/test.py)
 
 set_property(
     TEST
-        pytest
+        main_test
     PROPERTY
         ENVIRONMENT PROJECT_BUILD_DIR=${PROJECT_BINARY_DIR}
     )
 
 set_property(
     TEST
-        pytest
+        main_test
     APPEND
     PROPERTY
         ENVIRONMENT PROJECT_INCLUDE_DIR=${PROJECT_SOURCE_DIR}/api
@@ -19,7 +19,7 @@ set_property(
 
 set_property(
     TEST
-        pytest
+        main_test
     APPEND
     PROPERTY
         ENVIRONMENT PYTHONPATH=${PROJECT_SOURCE_DIR}/api
