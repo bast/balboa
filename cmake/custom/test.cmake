3d21488d0fb12ce915920b06a81743886f3a3963
Radovan Bast <bast@users.noreply.github.com>
2018-01-13 18:38:52 +0100

    introduce __init__.py

--- cmake/custom/test.cmake
+++ cmake/custom/test.cmake
@@ -1,20 +1,35 @@
 enable_testing()
 
-add_test(test_main py.test -vv -s ${PROJECT_SOURCE_DIR}/test/test.py)
-
-set_property(
-    TEST
+add_test(
+    NAME
         test_main
-    PROPERTY
-        ENVIRONMENT BALBOA_BUILD_DIR=${PROJECT_BINARY_DIR}
+    COMMAND
+        py.test -vv -s ${PROJECT_SOURCE_DIR}/test/test.py
     )
 
 set_property(
     TEST
         test_main
-    APPEND
     PROPERTY
-        ENVIRONMENT PYTHONPATH=${PROJECT_SOURCE_DIR}/balboa
+        ENVIRONMENT
+            BALBOA_LIBRARY_DIR=${PROJECT_BINARY_DIR}/lib
+            BALBOA_INCLUDE_DIR=${PROJECT_SOURCE_DIR}/balboa
+            PYTHONPATH=${PROJECT_SOURCE_DIR}
     )
 
-add_test(test_generate py.test -vv -s ${PROJECT_SOURCE_DIR}/balboa/generate.py)
+add_test(
+    NAME
+        test_generate
+    COMMAND
+        py.test -vv -s ${PROJECT_SOURCE_DIR}/balboa/generate.py
+    )
+
+set_property(
+    TEST
+        test_generate
+    PROPERTY
+        ENVIRONMENT
+            BALBOA_LIBRARY_DIR=${PROJECT_BINARY_DIR}/lib
+            BALBOA_INCLUDE_DIR=${PROJECT_SOURCE_DIR}/balboa
+            PYTHONPATH=${PROJECT_SOURCE_DIR}
+    )
