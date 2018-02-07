c300d0153dc9783243a67fb2e4675373cd8750a9
Radovan Bast <bast@users.noreply.github.com>
2018-02-07 10:40:03 +0100

    rm unused file

--- cmake/downloaded/autocmake_src.cmake
+++
@@ -1,8 +0,0 @@
-# (c) https://github.com/coderefinery/autocmake/blob/master/AUTHORS.md
-# licensed under BSD-3: https://github.com/coderefinery/autocmake/blob/master/LICENSE
-
-#.rst:
-#
-# Adds ${PROJECT_SOURCE_DIR}/src as subdirectory containing CMakeLists.txt.
-
-add_subdirectory(${PROJECT_SOURCE_DIR}/src)
