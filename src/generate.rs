2662a560aa579c1051b7bd9e45e08298d4a9160e
Radovan Bast <bast@users.noreply.github.com>
2020-02-22 15:52:20 +0100

    use tuples instead of vectors

--- src/generate.rs
+++ src/generate.rs
@@ -1,9 +1,9 @@
-fn get_ijk_list(m: usize) -> Vec<Vec<usize>> {
+pub fn get_ijk_list(m: usize) -> Vec<(usize, usize, usize)> {
     let mut l = Vec::new();
 
     for a in 1..(m + 2) {
         for b in 1..(a + 1) {
-            l.push(vec![m + 1 - a, a - b, b - 1]);
+            l.push((m + 1 - a, a - b, b - 1));
         }
     }
 
@@ -17,16 +17,16 @@ mod tests {
     #[test]
     fn test_ijk() {
         let reference = vec![
-            vec![3, 0, 0],
-            vec![2, 1, 0],
-            vec![2, 0, 1],
-            vec![1, 2, 0],
-            vec![1, 1, 1],
-            vec![1, 0, 2],
-            vec![0, 3, 0],
-            vec![0, 2, 1],
-            vec![0, 1, 2],
-            vec![0, 0, 3],
+            (3, 0, 0),
+            (2, 1, 0),
+            (2, 0, 1),
+            (1, 2, 0),
+            (1, 1, 1),
+            (1, 0, 2),
+            (0, 3, 0),
+            (0, 2, 1),
+            (0, 1, 2),
+            (0, 0, 3),
         ];
         assert_eq!(reference, get_ijk_list(3));
     }
