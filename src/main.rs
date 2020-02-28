f587c1e4aeabf10b54c429cd4233ad6259bb3f8e
Radovan Bast <bast@users.noreply.github.com>
2020-02-28 18:28:56 +0100

    time evaluation of aos

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use balboa;
+use std::time::Instant;
 
 fn main() {
     let mut rng = balboa::get_rng([0; 32]);
-    let num_points = 10;
+    let num_points = 10_000;
     let side_length = 2.0;
-    let _points = balboa::random_points(&mut rng, num_points, side_length);
+    let points_bohr = balboa::random_points(&mut rng, num_points, side_length);
+
+    let basis = balboa::example_basis();
+
+    let c_to_s_matrices = balboa::cartesian_to_spherical_matrices();
+
+    let start = Instant::now();
+    let _aos = balboa::get_ao_noddy(points_bohr, &basis, &c_to_s_matrices);
+    println!("time elapsed in get_ao_noddy: {:?}", start.elapsed());
 }
