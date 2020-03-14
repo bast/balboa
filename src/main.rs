bc04e6ee8cb35f319449793b6e40ce3e22e042f9
Radovan Bast <bast@users.noreply.github.com>
2020-03-14 20:43:12 +0100

    reduce size of benchmark

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 
 fn main() {
     let mut rng = balboa::get_rng([0; 32]);
-    let num_points = 500_000;
+    let num_points = 50_000;
     let side_length = 2.0;
     let points_bohr = balboa::random_points(&mut rng, num_points, side_length);
 
@@ -12,6 +12,6 @@ fn main() {
     let c_to_s_matrices = balboa::cartesian_to_spherical_matrices();
 
     let start = Instant::now();
-    let _aos = balboa::aos_noddy(0, &points_bohr, &basis, &c_to_s_matrices);
+    let _aos = balboa::aos_noddy(2, &points_bohr, &basis, &c_to_s_matrices);
     println!("time elapsed in aos_noddy: {:?}", start.elapsed());
 }
